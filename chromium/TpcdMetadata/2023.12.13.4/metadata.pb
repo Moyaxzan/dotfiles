0c623bef04392c75ebcad5a04982e6eea6ca7b31
Moyaxzan <tao.spa2003@gmail.com>
2023-12-13 20:08:27 +0100

    full arch config

---
+++ chromium/TpcdMetadata/2023.12.13.4/metadata.pb
@@ -0,0 +1,15 @@
+
+P
+*[*.]bounce-tracking-demo-tracker.glitch.me"[*.]bounce-tracking-demo.glitch.me
+-
+*(https://misty-sand-bellusaurus.glitch.me
+
+*[*.]newvoicemedia.com
+
+*https://[*.]salesforce.com
+"
+*https://[*.]my-salesforce.com
+
+*www.figma.com
+
+*staging.figma.com
---
+++ hypr/scripts/wallpaper.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#                _ _                              
+# __      ____ _| | |_ __   __ _ _ __   ___ _ __  
+# \ \ /\ / / _` | | | '_ \ / _` | '_ \ / _ \ '__| 
+#  \ V  V / (_| | | | |_) | (_| | |_) |  __/ |    
+#   \_/\_/ \__,_|_|_| .__/ \__,_| .__/ \___|_|    
+#                   |_|         |_|               
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+case $1 in
+
+    # Load wallpaper from .cache of last session 
+    "init")
+        if [ -f ~/.cache/wallpaper/current_wallpaper.jpg ]; then
+            rm ~/.cache/wal/schemes/*current_wallpaper_jpg*
+            wal -q -i ~/.cache/wallpaper/current_wallpaper.jpg
+        else
+            wal -q -i ~/wallpaper/
+        fi
+    ;;
+
+    # Randomly select wallpaper 
+    *)
+        wal -q -i ~/wallpaper/
+    ;;
+
+esac
+
+# ----------------------------------------------------- 
+# Load current pywal color scheme
+# ----------------------------------------------------- 
+source "$HOME/.cache/wal/colors.sh"
+echo "Wallpaper: $wallpaper"
+
+# ----------------------------------------------------- 
+# Copy selected wallpaper into .cache folder
+# ----------------------------------------------------- 
+cp $wallpaper ~/.cache/wallpaper/current_wallpaper.jpg
+
+# ----------------------------------------------------- 
+# get wallpaper image name
+# ----------------------------------------------------- 
+newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+
+# ----------------------------------------------------- 
+# Reload waybar with new colors
+# -----------------------------------------------------
+~/.config/waybar/launch.sh
+
+# ----------------------------------------------------- 
+# Set the new wallpaper
+# -----------------------------------------------------
+transition_type="wipe"
+# transition_type="outer"
+# transition_type="random"
+
+swww img $wallpaper \
+    --transition-bezier .43,1.19,1,.4 \
+    --transition-fps=60 \
+    --transition-type=$transition_type \
+    --transition-duration=0.7 \
+    --transition-pos "$( hyprctl cursorpos )"
+
+# ----------------------------------------------------- 
+# Send notification
+# ----------------------------------------------------- 
+sleep 1
+notify-send "Colors and Wallpaper updated" "with image $newwall"
+
+echo "DONE!"
